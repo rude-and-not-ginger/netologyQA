03c9ad31fb482e4732e083e31a8b6eecb5375ff6
Daria Kutsenko <da.ol.fil@gmail.com>
2023-02-12 18:21:48 +0100

    add jacoco remove trash

--- src/main/java/org/example/bonus/Miles.java
+++
@@ -1,9 +0,0 @@
-package org.example.bonus;
-
-public class Miles {
-    public int calculate(int spentMoney) {
-    int milesPerMoney = 20;
-    int acquieredMiles = spentMoney / milesPerMoney;
-    return acquieredMiles;
-    }
-}
